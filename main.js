210c83c0002c6a06a1f0edd844d040a25bc7b45a
Kenny <101839505+khui0@users.noreply.github.com>
2022-09-10 10:46:16 -0400

    Panels working properly

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const output = document.getElementById("output");
-output.offscreen = document.createElement("canvas");
 const w = 1920;
 const h = 1080;
 
 output.setAttribute("width", w);
 output.setAttribute("height", h);
+
+output.offscreen = document.createElement("canvas");
 output.offscreen.width = output.width;
 output.offscreen.height = output.height;
 
+output.temp = document.createElement("canvas");
+output.temp.width = output.width;
+output.temp.height = output.height;
+
 const ctx = output.offscreen.getContext("2d");
+const temp = output.temp.getContext("2d");
 
 const textSize = document.getElementById("text-size");
 
@@ -45,52 +51,28 @@ function update() {
 
     // Draw title
     ctx.font = `${(h / 7.5)}px Title`;
-    floating3DText("Upcoming Events", w * 0.5, h * 0.1, h / 70);
+    ctx.extrudedText("Upcoming Events", w * 0.5, h * 0.1, h / 70);
 
-    drawPanel(30, 200, 15);
+    // Draw panels to a temporary canvas
+    drawPanels(30, h * 0.2, 185, 4);
+
+    // Set opacity and draw to main canvas
+    ctx.globalAlpha = 0.5;
+    ctx.drawImage(output.temp, 0, 0);
+    ctx.globalAlpha = 1;
 
     // Draw offscreen canvas to onscreen canvas
     output.getContext("2d").drawImage(output.offscreen, 0, 0);
 }
 
-function floating3DText(string, x, y, depth) {
-    let startX = x + depth;
-    let startY = y + depth;
-    for (let i = 1; i < depth; i++) {
-        if (i == 1) {
-            ctx.shadowColor = "rgba(0, 0, 10, 1)";
-            ctx.shadowBlur = 30;
-            ctx.shadowOffsetX = 5;
-            ctx.shadowOffsetY = 5;
-        }
-        ctx.fillStyle = "rgb(30, 30, 30)";
-        ctx.fillText(string, startX - i, startY - i);
-        ctx.shadowColor = "transparent";
+function drawPanels(margin, top, height, count) {
+    for (let i = 0; i < count; i++) {
+        temp.drawPanel(margin, top + i * (height + margin), w - 2 * margin, height, 10);
     }
-    ctx.fillStyle = "white";
-    ctx.fillText(string, x, y);
-}
-
-function drawPanel(x, y, depth) {
-    let startX = x + depth;
-    let startY = y + depth;
-    for (let i = 1; i < depth; i++) {
-        ctx.fillStyle = "rgba(99, 18, 19, 0.5)";
-        ctx.roundRect(startX - i, startY - i, 1870, 200, 30).fill();
-    }
-    ctx.fillStyle = "rgba(116, 22, 22, 0.5)";
-    ctx.roundRect(x, y, 1870, 200, 30).fill();
 }
 
 function dateToString(date) {
     return new Date(date).toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric" });
-}
-
-function dateToISO(date) {
-    let year = date.getFullYear()
-    let month = (date.getMonth() + 1).toString().padStart(2, "0");
-    let day = date.getDate().toString().padStart(2, "0");
-    return `${year}-${month}-${day}`;
 }
 
 function downloadImage() {
@@ -98,6 +80,36 @@ function downloadImage() {
     link.download = `fr-events.png`;
     link.href = output.toDataURL();
     link.click();
+}
+
+CanvasRenderingContext2D.prototype.extrudedText = function (string, x, y, depth) {
+    let startX = x + depth;
+    let startY = y + depth;
+    for (let i = 1; i < depth; i++) {
+        if (i == 1) {
+            this.shadowColor = "rgba(0, 0, 10, 1)";
+            this.shadowBlur = 30;
+            this.shadowOffsetX = 5;
+            this.shadowOffsetY = 5;
+        }
+        this.fillStyle = "rgb(30, 30, 30)";
+        this.fillText(string, startX - i, startY - i);
+        this.shadowColor = "transparent";
+    }
+    this.fillStyle = "white";
+    this.fillText(string, x, y);
+}
+
+CanvasRenderingContext2D.prototype.drawPanel = function (x, y, w, h, depth) {
+    let startX = x + depth;
+    let startY = y + depth;
+    for (let i = 1; i < depth; i++) {
+        this.fillStyle = "rgba(99, 18, 19)";
+        this.roundRect(startX - i, startY - i, w, h, 30).fill();
+    }
+    this.fillStyle = "rgba(116, 22, 22)";
+    this.roundRect(x, y, w, h, 30).fill();
+    return this;
 }
 
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
